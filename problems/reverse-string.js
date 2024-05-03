499e028899d87d74f48d7c3a5c1e55fdd97e338f
sido <imbilusydney09@gmail.com>
2024-05-03 20:51:25 +0300

    wrote tests for reverse-string and wrote reverseString functio

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,11 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  if (typeof string !== 'string') {
+    throw new TypeError;
+  }
+  let newStr = '';
+  let n = string.length - 1
+  for (let i = n; i >= 0; i--) {
+    newStr += string[i];
+  }
+  return newStr;
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,17 @@
-// Your code here
+const {expect} = require('chai');
+const reverseString = require('../problems/reverse-string');
+
+describe('reverseString function', () => {
+    it('should reverse a string passed into it', () => {
+        let nuf = reverseString('fun');
+
+        expect(nuf).to.equal('nuf');
+    })
+
+    it('should throw a TypeError if not given a string', () => {
+        let obj = {'one': 1, 'two': 2, 'three': 3}
+
+        expect(() => reverseString(12)).to.throw(TypeError);
+        expect(() => reverseString(obj)).to.throw(TypeError);
+    });
+});
