22d1966e2a74a98aeec3a8fa49f0d0f1837ed08b
sido <imbilusydney09@gmail.com>
2024-05-03 21:22:54 +0300

    wrote spec and tests for number-fun

--- problems/number-fun.js
+++ problems/number-fun.js
@@ -1,9 +1,12 @@
 function returnsThree() {
-  // Your code here
+  return 3;
 }
 
 function reciprocal(n) {
-  // Your code here
+  if (n < 1 || n > 1000000) {
+    throw new RangeError('only accepts values from 1 to 1,000,000 inclusive');
+  }
+  return 1/n;
 }
 
 module.exports = {
--- test/number-fun-spec.js
+++ test/number-fun-spec.js
@@ -1,1 +1,41 @@
-// Your code here
+const { expect } = require('chai');
+const { returnsThree } = require('../problems/number-fun');
+const { reciprocal } = require('../problems/number-fun');
+
+describe('returnsThree function', () => {
+    it('should return the number 3', () => {
+        let num = returnsThree();
+        expect(num).to.equal(3);
+    });
+});
+
+describe('reciprocal function', () => {
+
+    context('valid arguments', () => {
+        it('should return the reciprocal of the number passed in', () => {
+            let num1 = 100;
+            let num2 = 5;
+            let num3 = 6000;
+
+            let rec1 = reciprocal(num1);
+            let rec2 = reciprocal(num2);
+            let rec3 = reciprocal(num3);
+
+            expect(rec1).to.equal(1/100);
+            expect(rec2).to.equal(1/5);
+            expect(rec3).to.equal(1/6000);
+        });
+    });
+
+    context('invalid arguments', () => {
+        it('should throw RangeError for invalid arguments', () => {
+            let badNum1 = 1000001;
+            let badNum2 = -1;
+            let badNum3 = 0;
+
+            expect(() => reciprocal(badNum1)).to.throw(RangeError);
+            expect(() => reciprocal(badNum2)).to.throw(RangeError);
+            expect(() => reciprocal(badNum3)).to.throw(RangeError);
+        });
+    });
+});
