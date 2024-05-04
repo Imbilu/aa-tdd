66a859b0075441c34da464df36c9ea53db28b45d
sido <imbilusydney09@gmail.com>
2024-05-04 13:01:43 +0300

    wrote spec and code for person class

--- test/triangle-spec.js
+++ test/triangle-spec.js
@@ -1,1 +1,20 @@
-// Your code here
+const { expect } = require('chai');
+const {Triangle, Isosceles, Scalene} = require('../problems/triangle');
+
+describe('triangle class', () => {
+    describe('constructor function', () => {
+        let tri;
+
+        beforeEach(() => {
+            tri = new Triangle(3, 4, 5);
+        });
+
+        it('should create an instance with properties side1, side2, side3', () => {
+            expect(tri).to.have.property(side1);
+            expect(tri).to.have.property(side2);
+            expect(tri).to.have.property(side3);
+        });
+
+        //it('should set triangle properties to the ');
+    });
+});
