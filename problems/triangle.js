cb323d26c7ce5cfc43b22c36b3ff21a645f4563f
sido <imbilusydney09@gmail.com>
2024-05-04 13:45:48 +0300

    wrote spec and code for triangle

--- problems/triangle.js
+++ problems/triangle.js
@@ -1,1 +1,34 @@
-// Your code here
+class Triangle {
+    constructor(side1, side2, side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    getPerimeter() {
+        return this.side1 + this.side2 + this.side3;
+    }
+
+    hasValidSideLengths() {
+        let s1 = this.side1;
+        let s2 = this.side2;
+        let s3 = this.side3;
+        return (s1 + s2 > s3 && s2 + s3 > s1 && s1 + s3 > s2);
+    }
+
+    validate() {
+        this.isValid = this.hasValidSideLengths();
+    }
+}
+
+class Scalene extends Triangle {
+
+}
+
+
+class Isosceles extends Triangle {
+
+}
+
+
+module.exports = { Triangle };
--- test/triangle-spec.js
+++ test/triangle-spec.js
@@ -2,19 +2,55 @@ const { expect } = require('chai');
 const {Triangle, Isosceles, Scalene} = require('../problems/triangle');
 
 describe('triangle class', () => {
-    describe('constructor function', () => {
-        let tri;
 
-        beforeEach(() => {
-            tri = new Triangle(3, 4, 5);
-        });
+    let tri;
+    let badTri;
+
+    beforeEach(() => {
+        tri = new Triangle(3, 4, 5);
+        badTri = new Triangle(2, 2, 5);
+    });
+
+    describe('constructor function', () => {
+
 
         it('should create an instance with properties side1, side2, side3', () => {
-            expect(tri).to.have.property(side1);
-            expect(tri).to.have.property(side2);
-            expect(tri).to.have.property(side3);
+            expect(tri).to.have.property('side1');
+            expect(tri).to.have.property('side2');
+            expect(tri).to.have.property('side3');
         });
 
-        //it('should set triangle properties to the ');
+        it('should set triangle properties to the passed in values', () => {
+            expect(tri.side1).to.eq(3);
+            expect(tri.side2).to.eq(4);
+            expect(tri.side3).to.eq(5);
+        });
+    });
+
+    describe('getPerimeter function', () => {
+        it('should return the perimeter of the triangle instance', () => {
+            let p = tri.getPerimeter();
+            expect(p).to.eq(12);
+        });
+    });
+
+    describe('hasValidSidelengths function', () => {
+        it('should return true if the sum of any two sides is greater than the remaining side', () => {
+            let validity = tri.hasValidSideLengths();
+            let validity2 = badTri.hasValidSideLengths();
+
+            expect(validity).to.eq(true);
+            expect(validity2).to.eq(false);
+        });
+    });
+
+    describe('validate method', () => {
+        it('adds an isValid property to instance with a value of true if the triangle is valid', () => {
+            tri.validate();
+            badTri.validate();
+
+            expect(tri.isValid).to.equal(true);
+            expect(badTri.isValid).to.eq(false);
+        })
     });
 });
